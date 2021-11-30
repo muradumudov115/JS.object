a314dab9ec45ccbe682875d37a5ea87bf7a10c91
muradumudov115 <91114208+muradumudov115@users.noreply.github.com>
2021-12-01 01:18:10 +0400

    object.task

---
+++ JS.object/script.js
@@ -0,0 +1,19 @@
+
+let company={
+    name: "Besiktas",
+    createYear= "1903"
+}
+
+let team={
+    name: "Galatasaray",
+    createYear: 1905
+}
+
+let eat={
+    name: "Pringles",
+    createYear:1998
+}
+
+let arr=[company,team,Eat] ;
+let checkAll=arr.forEach(x=> x.name=="Pringles");
+console.log(check);
